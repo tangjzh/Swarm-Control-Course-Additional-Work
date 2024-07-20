0c263f182b101cca827d1f77e952b332da5c3167
tangjzh <tangjzh.ai@gmail.com>
2024-07-20 20:12:04 +0800

    modified:   goal_publisher.py 	modified:   result_4x.gif

--- goal_publisher.py
+++ goal_publisher.py
@@ -13,17 +13,8 @@ def send_nav_goal(position_x):
     # 初始化节点
     rospy.init_node('send_nav_goals_node', anonymous=True)
     
-    # 创建一个发布器，发布到 /move_base_simple/goal 话题
+    # 创建一个发布器，发布到 /move_base_simple/goal 话题fabu
     goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
-
-    goal = PoseStamped()
-    goal.header.frame_id = "map"  # 使用地图坐标系
-    goal.header.stamp = rospy.Time.now()
-
-    # 设置位置，每次向左移动一定距离，假设每次移动1米
-    goal.pose.position.x = -22
-    goal.pose.position.y = 0.0
-    goal.pose.position.z = 0.0
 
     # goal_pub.publish(goal)
     rospy.sleep(2.0)
@@ -49,11 +40,9 @@ def send_nav_goal(position_x):
         goal_pub.publish(goal)
         rospy.loginfo("Goal {} sent: x={}, y={}".format(i+1, goal.pose.position.x, goal.pose.position.y))
         
-        # 等待一段时间，例如1秒
-        if i == 0:
-            rospy.sleep(6)
-        else:
-            rospy.sleep(15)
+        sleep_time = [7, 11.8, 10, 9.5, 0]
+        # 等待一段时间
+        rospy.sleep(sleep_time[i])
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
